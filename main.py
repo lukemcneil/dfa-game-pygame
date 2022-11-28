999660be697d4aa676451d9912ed1d27eb5de63e
Luke McNeil <lukemcneil61@gmail.com>
2022-11-27 19:02:01 -0500

    just draw every time in main loop

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ BACKGROUND_COLOR = [0, 0, 0]
 
 class State:
 	def __init__(self, position):
-		self.position : tuple[int, int] = position
+		self.position : list[int, int] = position
 		self.selected : bool = False
 
 class Edge:
@@ -86,12 +86,11 @@ while not exit:
 		elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: #left click
 			mouse_position = pygame.mouse.get_pos()
 			if getCircleCollidingWith(mouse_position):
-				new_state = State(mouse_position)
+				new_state = State(list(mouse_position))
 				states.append(new_state)
 			if clicked_circle:
 				clicked_circle.selected = False
 				clicked_circle = None
-			drawDFA()
 		elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3: #right click
 			mouse_position = pygame.mouse.get_pos()
 			if clicked_circle:
@@ -104,12 +103,11 @@ while not exit:
 				clicked_circle = getCircleAtPosition(mouse_position)
 				if clicked_circle:
 					clicked_circle.selected = True
-			drawDFA()
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_0:
 				selected_letter = "0"
 			elif event.key == pygame.K_1:
 				selected_letter = "1"
-			drawDFA()
+	drawDFA()
 
 	pygame.display.update()
