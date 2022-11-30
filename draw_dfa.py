71cf9b8f81340167d8770c388e070c4e659383e6
Luke McNeil <lukemcneil61@gmail.com>
2022-11-29 23:47:39 -0500

    shift arrows pointing back at each other

--- draw_dfa.py
+++ draw_dfa.py
@@ -12,6 +12,7 @@ LINE_COLOR = [255,0,255]
 BACKGROUND_COLOR = [0, 0, 0]
 ARROW_HEIGHT = 10
 ARROW_WIDTH = 10
+POINTING_BACK_SHIFT = 10
 FONT = pygame.font.SysFont("", 25)
 
 def drawStates(canvas, dfa):
@@ -39,16 +40,22 @@ def drawEdges(canvas, dfa):
 				text_position = [self_arrow_center[0]-RADIUS/3, self_arrow_center[1] - RADIUS * 2]
 				show_text(canvas, letter, LINE_COLOR, text_position)
 			else:
-				pygame.draw.line(canvas, LINE_COLOR, start, end, width=1)
+				is_edge_pointing_back = False
+				for check_state in to_state.edges.values():
+					if (check_state == state):
+						is_edge_pointing_back = True
 				dy = end[1] - start[1]
 				dx = end[0] - start[0]
 				angle = math.atan2(-dy, dx)
+				def shiftForPointingBack(point):
+					return translatePoint(point, angle+math.pi/2, POINTING_BACK_SHIFT if is_edge_pointing_back else 0)
+				pygame.draw.line(canvas, LINE_COLOR, shiftForPointingBack(start), shiftForPointingBack(end), width=1)
 				point1 = translatePoint(end, angle, RADIUS)
 				point2 = translatePoint(translatePoint(point1, angle - math.pi/2, ARROW_HEIGHT), angle, ARROW_WIDTH)
 				point3 = translatePoint(translatePoint(point1, angle + math.pi/2, ARROW_HEIGHT), angle, ARROW_WIDTH)
-				pygame.draw.polygon(canvas, LINE_COLOR, [point1, point2, point3])
+				pygame.draw.polygon(canvas, LINE_COLOR, [shiftForPointingBack(point1), shiftForPointingBack(point2), shiftForPointingBack(point3)])
 				midpoint = [(start[0] + end[0]) / 2, (start[1] + end[1]) / 2]
-				show_text(canvas, letter, LINE_COLOR, midpoint)
+				show_text(canvas, letter, LINE_COLOR, shiftForPointingBack(midpoint))
 
 def drawSelectedLetter(canvas, selected_letter):
 	show_text(canvas, "letter: " + selected_letter, LINE_COLOR, [10, 10])
